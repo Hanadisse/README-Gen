303edba05b70ead118f486561f622574a094a8c1
hanad isse <hanadisse1@gmail.com>
2022-12-05 09:45:28 -0600

    added string templates and did functions for rendering based on user input

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,95 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(data) {
+  const licensePicked = data.license;
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+  let licenseBadge = "";
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+  if (licensePicked === "MIT") {
+    licenseBadge = "[![License](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+    return licenseBadge;
+  }
+  if (licensePicked === "Apache") {
+    licenseBadge =
+"[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+    return licenseBadge;
+  }
+  if (licensePicked === "Boost") {
+    licenseBadge =
+"[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)";
+    return licenseBadge;
+  }
+  if (licensePicked === "BDS 3-Clause") {
+    licenseBadge =
+"[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)";
+    return licenseBadge;
+  } if (licensePicked === 'None') {
+    return licenseBadge;
+  }
+
+}
+
+const renderLicenseTableOfContents = (data) => {
+  const licensePicked = data.license;
+
+  if (licensePicked === 'None') {
+    return "";
+
+  } else {
+    return "* [License](#license)";
+  }
+};
+
+function renderLicenseSection(data) {
+  const licensePicked = data.license;
+
+  if (licensePicked === 'None') {
+    return "";
+
+  } else {
+    return `## License
+${renderLicenseBadge(data)}`;
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+## Table of Contents
+
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+${renderLicenseTableOfContents(data)}
+* [Questions](#questions)
+* [Test](#test)
+
+## Description
+${data.description}
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## Credits
+${data.credits}
+
+${renderLicenseSection(data)}
+
+## Questions
+${data.questions}
+
+## Test
+${data.test}
 `;
+
 }
+
+
+
 
 module.exports = generateMarkdown;
